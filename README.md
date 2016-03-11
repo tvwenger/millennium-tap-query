014525093e8173e87d8bdbb5c914dd6cf96a5b9d
Trey Wenger <tvwenger@gmail.com>
2016-03-11 18:00:29 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # millennium-tap-query
-Query the Millennium Simulation UWS/TAP client
+A Python Tool to Query the Millennium Simulation UWS/TAP client
 
 This is a simple wrapper for the Python package `requests` to deal
 with connections to the [Millennium TAP Web
@@ -7,13 +7,28 @@ Client](http://galformod.mpa-garching.mpg.de/millenniumtap/).  With
 this tool you can perform basic or advanced queries to the Millennium
 Simulation database and download the data products.
 
+This tool is similar to the [TAP
+query](http://svn.ari.uni-heidelberg.de/svn/gavo/python/trunk/docs/tapquery.rstx)
+tool in the German Astrophysical Virtual Observatory (GAVO) VOtables
+package.
+
+## Prerequisites
+
+This program is tested on both Python 2.7 and Python 3.5. You can
+install the required prerequisites using `pip` via
+```bash
+pip install -r requirements.txt
+```
+
 ## Usage Examples
 ```python
 """
 Set up a new job, perform a simple query, and save the results
 """
 import millennium_query
-conn = millennium_query.MillenniumQuery('username','password')
+username='foo' # replace with your login credentials
+password='bar'
+conn = millennium_query.MillenniumQuery(username,password)
 conn.query('SELECT TOP 10 * FROM MPAGalaxies..DeLucia2006a')
 conn.run() # Start job
 conn.wait() # Wait until job is finished
@@ -26,19 +41,20 @@ conn.close() # close connection
 Connect to an already submitted job and save the results
 """
 job_id = 'foobar'
-conn = millennium_query.MillenniumQuery('username','password',job_id=job_id)
+conn = millennium_query.MillenniumQuery(username,password,job_id=job_id)
 conn.wait() # Make sure job is done
 conn.save('results.csv')
+conn.close()
 ```
 
 ```python
 """
 Acquire cookies, then run several jobs
 """
-cookies = millennium_query.get_cookies('username','password')
+cookies = millennium_query.get_cookies(username,password)
 jobs = []
-for i in xrange(10):
-    conn = millennium_query.MillenniumQuery('username','password',cookies=cookies)
+for i in range(10):
+    conn = millennium_query.MillenniumQuery(username,password,cookies=cookies)
     conn.query('SELECT TOP {0} * FROM MPAGalaxies..DeLucia2006a'.format(i))
     conn.run()
     jobs.append(conn)
@@ -47,3 +63,78 @@ for i,conn in enumerate(jobs):
     conn.save('results_{0}.csv'.format(i))
     conn.close()
 ```
+
+## Details
+
+This tool contains two standalone functions and the `MillenniumQuery`
+class.
+
+`get_response` performs the actual query to the TAP client. It takes
+two arguments: `session` is a `requests.Session()` object and `url` is
+the URL to query. The keyword arguments are `method` (default is
+`'GET'`) determines whether the query should use GET or POST, `data`
+(default is `None`) is the POST or GET data, `cookies` (default is
+`None`), `max_attempts` (default is `5`), and stream (default is
+`False`). This function expects `session.auth` to be appropriately set
+already. It will perform the query up to `max_attempts` times before
+giving up and raising a `RuntimeError`. It will also raise a
+`RuntimeError` if it does not get the expected XML webpage (usually a
+sign that the login credentials are incorrect). If successful, it
+returns the `response` returned by the call to `session.get()` or
+`session.post()`.
+
+`get_cookies` performs a simple connect to download site cookies.  It
+takes two arguments: `username` and `password`, your login credentials
+to the Millennium TAP client. The function returns a
+`RequestsCookieJar` object.
+
+The constructor for the `MillenniumQuery` object also takes the
+`username` and `password` arguments, as well as the following keyword
+arguments: `job_id` (default is `None`) is set if you wish to connect
+to a previously submitted job, `query_lang` (default is `SQL`) to set
+the query language, `results_format` (default is `csv`) sets the
+TAP-returned filetype, `max_rec` (default is `100000`) is the maximum
+number of records to retrieve, and `cookies` (default is None). For
+more details on these parameters, see the [MillenniumTAP help
+page](http://galformod.mpa-garching.mpg.de/millenniumtap/tapface/pages/help.jsp).
+The constructor sets up the `requests.Session()` object and obtains
+the necessary cookies.
+
+`MillenniumQuery:query` takes one parameter, `query`. This query is
+sent to the TAP client. If no `job_id` was specified in the
+constructor, this function determines the `job_id` for this job.
+
+`MillenniumQuery:run` starts any queried job on the TAP client. 
+This function will raise `RuntimeError` if the `query` function has
+not already been called.
+
+`MillenniumQuery:wait` holds execution until the TAP client returns
+a `COMPLETED`, `ABORTED`, or `ERROR` status. The second two statuses
+will raise a `RuntimeError`. This function takes a keyword argument
+`max_attempts` (default is 100) that determines the maximum number of
+`while` iterations before raising a `RuntimeError`. The pause between
+each successive iteration grows exponential to a maximum of 2 minutes.
+
+`MillenniumQuery:save` streams the results file and saves to the a
+file defined by the argument `filename`. 
+
+`MillenniumQuery:close` closes the session.
+
+## Warranty and Copyright
+
+Copyright (C) 2016 by Trey Wenger: tvwenger@gmail.com
+
+GNU General Public License v3 (GNU GPLv3)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- millennium_query.py
+++ millennium_query.py
@@ -2,17 +2,25 @@
 millennium_query.py
 
 Query the Millennium Simulation UWS/TAP client.
-
 Similar to tapquery in gavo.votable
-
-GNU General Public License v3 (GNU GPLv3)
-You should have received a copy of the GNU General Public License
-along with this software.  If not, see <http://www.gnu.org/licenses/>.
-You may use, modify, and/or redistribute this software under the
-terms of the GNU General Public License version 3 or any future version.
 
 Copyright(C) 2016 by
 Trey Wenger; tvwenger@gmail.com
+
+GNU General Public License v3 (GNU GPLv3)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 11 Mar 2016 - TVW Finalized version 1.0
 """
@@ -26,15 +34,38 @@ from lxml import etree
 
 _MILL_URL = "http://galformod.mpa-garching.mpg.de/millenniumtap/async"
 
+def get_response(session,url,method='GET',data=None,
+                 cookies=None,max_attempts=5,stream=False):
+    """
+    Perform call to website. Check that an error wasn't thrown,
+    and check that we are logged in. Return response
+    """
+    n_attempts = 0
+    while n_attempts < max_attempts:
+        try:
+            if method == 'GET':
+                response = session.get(url,data=data,cookies=cookies,stream=stream)
+            else:
+                response = session.post(url,data=data,cookies=cookies,stream=stream)
+            break
+        except requests.exceptions.ConnectionError:
+            # try again
+            n_attempts += 1
+    else:
+        # it never worked...
+        raise RuntimeError("Problem connecting to {0} with method {1} and data {2}".format(url,method,data))
+    # it worked!
+    if "Login at Millennium TAP" in str(response.content):
+        raise RuntimeError("Login credentials not valid for MillenniumTAP")
+    return response
+
 def get_cookies(username,password):
     """
     Connect to the host just to get the login cookies
     """
     session = requests.Session()
     session.auth = (username,password)
-    response = session.get(_MILL_URL)
-    if "Login at Millennium TAP" in response.content:
-        raise RuntimeError("Login credentials not valid for MillenniumTAP")
+    response = get_response(session,_MILL_URL,method='GET')
     cookies = session.cookies
     session.close()
     return cookies
@@ -61,7 +92,7 @@ class MillenniumQuery:
         if self.job_id is not None:
             self.job_url = '{0}/{1}'.format(self.url,self.job_id)
         # Get cookies if we don't already have some
-        if self.cookies is None:
+        if cookies is None:
             self.cookies = get_cookies(username,password)
         else:
             self.cookies = cookies
@@ -81,12 +112,8 @@ class MillenniumQuery:
         post_data = self.post_data.copy()
         post_data['QUERY'] = query
         # Set up job
-        response = self.session.post(url,data=post_data,
-                                     cookies=self.cookies)
-        if "Login at Millennium TAP" in response.content:
-            raise RuntimeError("Login credentials not valid for MillenniumTAP")
-        # Save cookies in case we don't already have some
-        self.cookies = self.session.cookies
+        response = get_response(self.session,url,method='POST',
+                                data=post_data,cookies=self.cookies)
         # If we don't already have one, get Job ID from output XML
         if self.job_id is None:
             root = etree.fromstring(response.content)
@@ -103,17 +130,17 @@ class MillenniumQuery:
         url = '{0}/{1}'.format(self.job_url,'phase')
         post_data = self.post_data.copy()
         post_data['PHASE'] = 'RUN'
-        response = self.session.post(url,data=post_data,
-                                     cookies=self.cookies)
-        if "Login at Millennium TAP" in response.content:
-            raise RuntimeError("Login credentials not valid for MillenniumTAP")
+        response = get_response(self.session,url,method='POST',
+                                data=post_data,cookies=self.cookies)
 
     def wait(self,max_attempts=100):
         """
         Wait for job to finish, then get the results
         Throw error if waiting for too long
         """
-        niter = 0
+        attempts = 0
+        wait_time = 1. # starting wait time
+        increment = 2.**0.25 # magic number for wait time
         while True:
             phase = self.phase
             if phase == 'COMPLETED':
@@ -125,10 +152,10 @@ class MillenniumQuery:
             if max_attempts:
                 if niter > max_attempts:
                     raise RuntimeError("Did not get MillenniumQuery results after {0} iterations, job {1}".format(max_attempts,self.job_id))
-            niter += 1
-            # wait for 120 seconds or niter*2^(1/4) seconds, whichever
-            # is shorter
-            wait_time = 1.189207115002721*increment
+            attempts += 1
+            # wait for 120 seconds or 2^(niter/4) seconds,
+            # whichever is shorter
+            wait_time = min(120.,poll_interval*increment)
             time.sleep(min(120.,wait_time))
 
     def save(self,filename):
@@ -141,13 +168,12 @@ class MillenniumQuery:
             raise RuntimeError("Job {0} is not completed yet!".format(self.job_id))
         url = '{0}/results/result'.format(self.job_url)
         # Stream chunks from download since the file could be huge
-        response = self.session.get(url,cookies=self.cookies,stream=True)
-        if "Login at Millennium TAP" in response.content:
-            raise RuntimeError("Login credentials not valid for MillenniumTAP")
+        response = get_response(self.session,url,method='GET',
+                                cookies=self.cookies,stream=True)
         with open(filename,'w') as f:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk: # filter out "keep-alive" chunks
-                    f.write(chunk)
+                    f.write(str(chunk))
 
     def close(self):
         """
@@ -162,9 +188,8 @@ class MillenniumQuery:
         """
         if self.job_url is None:
             raise RuntimeError("Must first call query() to set up MillenniumQuery")
-        response = self.session.get(self.job_url,cookies=self.cookies)
-        if "Login at Millennium TAP" in response.content:
-            raise RuntimeError("Login credentials not valid for MillenniumTAP")
+        response = get_response(self.session,self.job_url,method='GET',
+                                cookies=self.cookies)
         root = etree.fromstring(response.content)
         phase = root.find('uws:phase',root.nsmap).text
         return phase
@@ -176,9 +201,8 @@ class MillenniumQuery:
         """
         if self.job_url is None:
             raise RuntimeError("Must first call query() to set up MillenniumQuery")
-        response = self.session.get(self.job_url,cookies=self.cookies)
-        if "Login at Millennium TAP" in response.content:
-            raise RuntimeError("Login credentials not valid for MillenniumTAP")
+        response = get_response(self.session,self.job_url,method='GET',
+                                cookies=self.cookies)
         root = etree.fromstring(response.content)
         message = root.find('uws:errorSummary/uws:message',root.nsmap).text
         return message
